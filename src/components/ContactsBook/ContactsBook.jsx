98a95ad7cc2f5b036da278e6bc413add2cbe4ea9
Sergey-Vil <vilchinskiy.s.st@gmail.com>
2023-02-06 22:25:31 +0200

    1

---
+++ src/components/ContactsBook/ContactsBook.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactsForm from './ContactsForm/ContactsForm';
+import ContactsList from './ContactsList/ContactsList';
+import ContactsFilter from './ContactsFilter/ContactsFilter';
+import css from './contactsBook.modale.css';
+
+export class ContactsBook extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  addContacts = data => {
+    const newContact = { ...data, id: nanoid() };
+
+    this.state.contacts.some(
+      contact =>
+        contact.name.toLowerCase().trim() ===
+          newContact.name.toLowerCase().trim() ||
+        contact.number.trim() === newContact.number.trim()
+    )
+      ? alert(`${newContact.name} already exists`)
+      : this.setState(prevState => ({
+          contacts: [...prevState.contacts, newContact],
+        }));
+  };
+  changeFilter = e => {
+    this.setState({ filter: e.target.value.trim().toString() });
+  };
+  findContacts = () => {
+    const { filter, contacts } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+  render() {
+    const { filter } = this.state;
+    return (
+      <div className={css.phonebook}>
+        <h2 className={css.title}>Phonebook</h2>
+        <ContactsForm onSubmit={this.addContacts} />
+        <h2 className={css.title}>Contacts</h2>
+        <ContactsFilter filter={filter} onChangeFilter={this.changeFilter} />
+        <ContactsList
+          contacts={this.findContacts()}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default ContactsBook;
---
+++ src/components/ContactsBook/ContactsForm/ContactsForm.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import css from './contacnsForm.module.css';
+
+const INITIAL_STATE = {
+  name: '',
+  number: '',
+};
+
+export class ContactsForm extends Component {
+  state = {
+    ...INITIAL_STATE,
+  };
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+  hanleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit({ ...this.state });
+    this.setState({ ...INITIAL_STATE });
+  };
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form onSubmit={this.hanleSubmit} className={css.form}>
+        <label className="css.label__name" htmlFor="name">
+          Name
+        </label>
+        <input
+          onChange={this.handleChange}
+          type="text"
+          minLength={3}
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          value={name}
+          required
+        />
+        <label className="css.label__name" htmlFor="number">
+          Number
+        </label>
+        <input
+          onChange={this.handleChange}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          value={number}
+          required
+        />
+        <button className={css.btn__submit} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+export default ContactsForm;
